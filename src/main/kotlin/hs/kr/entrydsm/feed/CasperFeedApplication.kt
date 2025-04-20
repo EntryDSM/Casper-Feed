da04d0791cfdda22f3a8e8e2b4314f06208a45a4
박주원 <qkrwndnjs7308@gmail.com>
2025-04-20 23:38:41 +0900

    feat ( #1 ) : 스프링부트 프로젝트 기본 설정

---
+++ src/main/kotlin/hs/kr/entrydsm/feed/CasperFeedApplication.kt
@@ -0,0 +1,11 @@
+package hs.kr.entrydsm.feed
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CasperFeedApplication
+
+fun main(args: Array<String>) {
+    runApplication<CasperFeedApplication>(*args)
+}
