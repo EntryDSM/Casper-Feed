72e3c5f1462ee2313a3de2f0536dcebf9554c5e5
coehgns <modooboiroo@gmail.com>
2025-07-29 17:16:20 +0900

    chore ( #14 ) : casper-feed 모듈 분리

--- src/main/kotlin/hs/kr/entrydsm/feed/domain/question/exception/AccessDeniedQuestionException.kt
+++
@@ -1,8 +0,0 @@
-package hs.kr.entrydsm.feed.domain.question.exception
-
-import hs.kr.entrydsm.feed.global.error.exception.EquusException
-import hs.kr.entrydsm.feed.global.error.exception.ErrorCode
-
-object AccessDeniedQuestionException : EquusException(
-    ErrorCode.ACCESS_DENIED_QUESTION
-)
--- src/main/kotlin/hs/kr/entrydsm/feed/infrastructure/feign/client/user/model/Admin.kt
+++
@@ -1,7 +0,0 @@
-package hs.kr.equus.feed.infrastructure.feign.client.user.model
-
-import java.util.UUID
-
-data class Admin(
-    val id: UUID
-)
