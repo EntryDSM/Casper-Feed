348f6f85b7e0d445f40a01e3d8cc212ec15cf0d0
coehgns <modooboiroo@gmail.com>
2025-09-02 20:26:42 +0900

    docs ( #34 ) : ResilienceGrpcExtensions에 kdoc 추가

--- casper-feed/src/main/kotlin/hs/kr/entrydsm/feed/global/extension/ResilienceGrpcExtensions.kt
+++ casper-feed/src/main/kotlin/hs/kr/entrydsm/feed/global/extension/ResilienceGrpcExtensions.kt
@@ -9,6 +9,16 @@ import org.slf4j.LoggerFactory
 
 private val log = LoggerFactory.getLogger("ResilienceGrpcExtensions")
 
+/**
+ * gRPC 호출에 대해 Retry와 CircuitBreaker를 적용합니다.
+ *
+ * @param T gRPC 함수의 반환 타입
+ * @param retry 적용할 Retry 인스턴스
+ * @param circuitBreaker 적용할 CircuitBreaker 인스턴스
+ * @param fallback gRPC 호출 실패 시 실행할 함수
+ * @param block 실행할 gRPC 함수
+ * @return gRPC 함수의 실행 결과를 반환하거나, 실패 시 fallback 함수의 결과를 반환
+ */
 suspend fun <T> executeGrpcCallWithResilience(
     retry: Retry,
     circuitBreaker: CircuitBreaker,
